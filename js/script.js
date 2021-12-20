2e628ba99a76902bc9a99ea203e973badf74f6c5
justt1n <t1nnguy3n0709@gmail.com>
2021-12-20 15:59:38 +0700

    xong html + css

--- js/script.js
+++ js/script.js
@@ -1,132 +1,148 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".navbar");
 
-menu.onclick = () =>{
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
-}
-
-document.querySelector('#login-btn').onclick = () =>{
-  document.querySelector('.login-form-container').classList.toggle('active');
-}
-
-document.querySelector('#close-login-form').onclick = () =>{
-  document.querySelector('.login-form-container').classList.remove('active');
-}
-
-window.onscroll = () =>{
-
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active');
-
-  if(window.scrollY > 0){
-    document.querySelector('.header').classList.add('active');
-  }else{
-    document.querySelector('.header').classList.remove('active');
-  };
-
+menu.onclick = () => {
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
 };
 
-document.querySelector('.home').onmousemove = (e) =>{
-
-  document.querySelectorAll('.home-parallax').forEach(elm =>{
-
-    let speed = elm.getAttribute('data-speed');
-
-    let x = (window.innerWidth - e.pageX * speed) / 90;
-    let y = (window.innerHeight - e.pageY * speed) / 90;
-
-    elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
-
-  });
-
+document.querySelector("#login-btn").onclick = () => {
+    document.querySelector(".login-form-container").classList.toggle("active");
 };
 
+document.querySelector("#close-login-form").onclick = () => {
+    document.querySelector(".login-form-container").classList.remove("active");
+};
 
-document.querySelector('.home').onmouseleave = (e) =>{
+window.onscroll = () => {
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
 
-  document.querySelectorAll('.home-parallax').forEach(elm =>{
+    if (window.scrollY > 0) {
+        document.querySelector(".header").classList.add("active");
+    } else {
+        document.querySelector(".header").classList.remove("active");
+    }
+};
 
-    elm.style.transform = `translateX(0px) translateY(0px)`;
+document.querySelector(".home").onmousemove = (e) => {
+    document.querySelectorAll(".home-parallax").forEach((elm) => {
+        let speed = elm.getAttribute("data-speed");
 
-  });
+        let x = (window.innerWidth - e.pageX * speed) / 90;
+        let y = (window.innerHeight - e.pageY * speed) / 90;
 
+        elm.style.transform = `translateX(${y}px) translateY(${x}px)`;
+    });
+};
+
+document.querySelector(".home").onmouseleave = (e) => {
+    document.querySelectorAll(".home-parallax").forEach((elm) => {
+        elm.style.transform = `translateX(0px) translateY(0px)`;
+    });
 };
 
 var swiper = new Swiper(".vehicles-slider", {
-  grabCursor: true,
-  centeredSlides: true,  
-  spaceBetween: 20,
-  loop:true,
-  autoplay: {
-    delay: 9500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable:true,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
+    grabCursor: true,
+    centeredSlides: true,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
     },
-    768: {
-      slidesPerView: 2,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
     },
-    1024: {
-      slidesPerView: 3,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
     },
-  },
 });
 
 var swiper = new Swiper(".featured-slider", {
-  grabCursor: true,
-  centeredSlides: true,  
-  spaceBetween: 20,
-  loop:true,
-  autoplay: {
-    delay: 9500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable:true,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
+    grabCursor: true,
+    centeredSlides: true,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
     },
-    768: {
-      slidesPerView: 2,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
     },
-    1024: {
-      slidesPerView: 3,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
     },
-  },
 });
 
 var swiper = new Swiper(".review-slider", {
-  grabCursor: true,
-  centeredSlides: true,  
-  spaceBetween: 20,
-  loop:true,
-  autoplay: {
-    delay: 9500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable:true,
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
+    grabCursor: true,
+    centeredSlides: true,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
     },
-    768: {
-      slidesPerView: 2,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
     },
-    1024: {
-      slidesPerView: 3,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
     },
-  },
 });
+
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate() {
+    console.log("ok nè");
+    var username = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    if (username == "tinlong@gmail.com" && password == "123456") {
+        alert("Login successfully");
+        window.location = "index.html"; // Redirecting to other page.
+        document.querySelector(".login-form-container").classList.toggle("logined");
+        return false;
+    } else {
+        attempt--; // Decrementing by one.
+        alert("You have left " + attempt + " attempt;");
+        alert(
+            "Try username: &quot;tinlong@gmail.com&quot; and password: &quot;123456&quot;"
+        );
+        // Disabling fields after 3 attempts.
+        if (attempt == 0) {
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+};
