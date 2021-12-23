7bbd286c13818006cb477342b33dd7304b6b30f7
justt1n <t1nnguy3n0709@gmail.com>
2021-12-23 16:53:05 +0700

    chinh js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-let menu = document.querySelector("#menu-btn");
-let navbar = document.querySelector(".navbar");
-
-menu.onclick = () => {
-    menu.classList.toggle("fa-times");
-    navbar.classList.toggle("active");
-};
-
 document.querySelector("#login-btn").onclick = () => {
     document.querySelector(".login-form-container").classList.toggle("active");
 };
@@ -15,8 +7,6 @@ document.querySelector("#close-login-form").onclick = () => {
 };
 
 window.onscroll = () => {
-    menu.classList.remove("fa-times");
-    navbar.classList.remove("active");
 
     if (window.scrollY > 0) {
         document.querySelector(".header").classList.add("active");
